7489c2dfe4ff3db37147be3a80411a513556b194
vk-vasanth-vk <vasanthakumar.devops@gmail.com>
2025-04-26 09:24:05 +0530

    Database connected

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+import authRoutes from "./routes/auth.js";
+app.use("/api", authRoutes);
+
+// DB Connect
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("MongoDB connected");
+}).catch((err) => console.log(err));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
